5e704eb5df8da9f8422f594334d47e9defa0ff0c
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-10-01 17:00:16 -0400

    Revert "EDE-9"

--- src/components/AppScreen.js
+++ src/components/AppScreen.js
@@ -1,25 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Flavors } from './Flavors'
 import { People } from './People'
 import { TotalFlavors } from './TotalFlavors'
-import { ModalPerson } from './ModalPerson'
-import { ModalFlavor } from './ModalFlavor'
 
 export const AppScreen = () => {
-    const [personModal, setPersonModal] = useState(false);
-    const [flavorModal, setFlavorModal] = useState(false);
-    const showPersonaModal = () => {
-        setPersonModal(true)
-    }
-    const hidePersonModal = () => {
-        setPersonModal(false)
-    }
-    const showFlavorModal = () => {
-        setFlavorModal(true)
-    }
-    const hideFlavorModal = () => {
-        setFlavorModal(false)
-    }
     const data = {
         names:
             [
@@ -50,19 +34,12 @@ export const AppScreen = () => {
         ]
     }
     return (
-        <>
-            {
-                personModal ? <ModalPerson hidePersonModal={hidePersonModal} /> :
-                    flavorModal ? <ModalFlavor hideFlavorModal={hideFlavorModal} /> : (
-                        <div className="container">
-                            <div className="flex bg-gray-100">
-                                <Flavors flavors={data.flavors} showFlavorModal={showFlavorModal} />
-                                <People data={data} showPersonaModal={showPersonaModal} />
-                                <TotalFlavors data={data} />
-                            </div>
-                        </div>
-                    )
-            }
-        </>
+        <div className="container">
+            <div className="flex bg-gray-100">
+                <Flavors flavors={data.flavors} />
+                <People data={data} />
+                <TotalFlavors data={data} />
+            </div>
+        </div>
     )
 }
--- src/components/ModalFlavor.js
+++ src/components/ModalFlavor.js
@@ -1,21 +1,9 @@
 import React from 'react'
 
-export const ModalFlavor = ({ hideFlavorModal }) => {
+export const ModalFlavor = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4 flex space-x-4">
-                    <svg className="h-10 w-10 text-gray-500 hover:text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-                    </svg>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="flavor" type="text" placeholder="flavor" />
-                </div>
-                <div className="flex items-center justify-center">
-                    <button onClick={hideFlavorModal} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" type="button">
-                        Add
-                    </button>
-                </div>
-            </form>
-        </div>
+        <>
+            
+        </>
     )
 }
