349105f743795eefeaa8ab69dd242f0090c6815c
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2020-10-08 09:29:54 -0300

    Revert "Revert "Revert "EDE-9"""

--- src/components/People.js
+++ src/components/People.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { PersonItem } from './PersonItem'
 
-export const People = ({ data, showPersonaModal }) => {
+export const People = ({ data }) => {
     const x = 3
     return (
         <div className="inline-block">
@@ -29,12 +30,13 @@ export const People = ({ data, showPersonaModal }) => {
                         </div>
                     </div>
                     <div className="bg-gray-300 h-12 flex items-center justify-center">
-                        <button onClick={showPersonaModal} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" type="button">
+                        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                             +
                         </button>
                     </div>
                 </div>
             }
         </div>
+
     )
 }
