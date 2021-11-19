6a9874ecd3430008f7a7fe21ebdc48c764b34ff8
Jorge Raúl Díaz Samaniego <jorgediazsamaniego@gmail.com>
2021-11-19 14:03:45 -0300

    update tailwind css CU-1r04jfd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import './styles/tailwind.output.css'
+import App from "./App";
+import "./styles/tailwind.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
